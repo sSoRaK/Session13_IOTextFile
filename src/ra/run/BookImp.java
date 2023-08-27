9b7769d41d953a2b7b296db2b0e07ac17d41f949
sSoRaK <thong9515@gmail.com>
2023-08-27 09:33:42 +0900

    modify methods

--- src/ra/run/BookImp.java
+++ src/ra/run/BookImp.java
@@ -13,11 +13,11 @@ public class BookImp implements Serializable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("************************* MENU ************************");
-            System.out.print("1. Nhập thông tin các sách");
-            System.out.print("2. In thông tin các sách ra file demo.txt");
-            System.out.print("3. Đọc file demo.txt và in ra các sách có giá trong khoảng 10000 đến 20000");
-            System.out.print("4. Thoát");
+            System.out.println("************************* MENU ************************");
+            System.out.println("1. Nhập thông tin các sách");
+            System.out.println("2. In thông tin các sách ra file demo.txt");
+            System.out.println("3. Đọc file demo.txt và in ra các sách có giá trong khoảng 10000 đến 20000");
+            System.out.println("4. Thoát");
             System.out.print("Nhập lựa chọn: ");
             int choice = Integer.parseInt(scanner.nextLine());
             switch (choice) {
@@ -25,8 +25,10 @@ public class BookImp implements Serializable {
                     BookImp.inputData();
                     break;
                 case 2:
+                    BookImp.saveToFile("demo.txt");
                     break;
                 case 3:
+                    BookImp.readFromFileAndPrintInRange("demo.txt", 10000, 20000);
                     break;
                 case 4:
                     System.exit(0);
@@ -43,7 +45,7 @@ public class BookImp implements Serializable {
         System.out.print("Nhập số lượng sách cần thêm: ");
         int numBook = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < numBook; i++) {
-            System.out.print("Nhập thông tin sách thứ " + (i + 1) + " : ");
+            System.out.println("Nhập thông tin sách thứ " + (i + 1) + " : ");
             Book book = new Book();
             book.inputData(scanner, bookList);
             bookList.add(book);
@@ -51,70 +53,42 @@ public class BookImp implements Serializable {
         System.out.println("Đã thêm sách thành công.");
     }
 
-    public static void writeDataToFile(List<Book> bookList) {
-        File file = new File("demo.txt");
-        FileOutputStream fos = null;
-        ObjectOutputStream oos = null;
-        try {
-            fos = new FileOutputStream(file);
-            oos = new ObjectOutputStream(fos);
-            oos.writeObject(bookList);
-            oos.flush();
-        } catch (FileNotFoundException e) {
-            System.err.println("File không tồn tại!");
-        } catch (IOException e) {
-            System.err.println("Lỗi ghi dữ liệu ra file!");
-        } catch (Exception e) {
-            System.err.println("Xảy ra lỗi trong quá trình ghi dữ liệu ra file!");
-        } finally {
-            try {
-                if (oos != null) {
-                    oos.close();
-                }
-                if (fos != null) {
-                    fos.close();
-                }
-            } catch (IOException e) {
-                System.err.println("Xảy ra lỗi khi đóng các stream!");
-            } catch (Exception e) {
-                System.err.println("Xảy ra lỗi trong quá trình đóng các stream!");
+    private static void saveToFile(String fileName) {
+        try (FileOutputStream fos = new FileOutputStream(fileName)) {
+            for (Book book : bookList) {
+                String bookInfo = book.getBookId() + "," + book.getBookName() + "," + book.getPrice() + "\n";
+                fos.write(bookInfo.getBytes());
             }
+            System.out.println("Ghi thông tin sách vào file thành công.");
+        } catch (IOException e) {
+            System.out.println("Lỗi: " + e.getMessage());
         }
     }
 
-    public static List<Book> readDataFromFile() {
-        File file = new File("demo.txt");
-        FileInputStream fis = null;
-        ObjectInputStream ois = null;
-        List<Book> bookListRead = null;
-        try {
-            // 2. khởi tạo đối tượng FileInputStream
-            fis = new FileInputStream(file);
-            // 3. khởi tạo đối tượng ObjectInputStream
-            ois = new ObjectInputStream(fis);
-            // 4. đọc dữ liệu Object từ file (readObject())
-            bookListRead = (List<Book>) ois.readObject();
-        } catch (FileNotFoundException e) {
-            System.err.println("Không tồn tại file!");
-        } catch (IOException e) {
-            System.err.println("Lỗi khi đọc file!");
-        } catch (Exception e) {
-            System.err.println("Có lỗi trong quá trình đọc dữ liệu từ file.");
-        } finally {
-            // 6. đóng các stream
-            try {
-                if (fis != null) {
-                    fis.close();
-                }
-                if (ois != null) {
-                    ois.close();
-                }
-            } catch (IOException e) {
-                System.err.println("Có lỗi khi đóng stream!");
-            } catch (Exception e) {
-                System.err.println("Có lỗi trong quá trình đóng các stream.");
+    private static void readFromFileAndPrintInRange(String fileName, float minPrice, float maxPrice) {
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            StringBuilder content = new StringBuilder();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                content.append(new String(buffer, 0, bytesRead));
             }
-            return bookListRead;
+
+            String[] lines = content.toString().split("\n");
+            System.out.println("Sách trong khoảng giá (10000 - 20000): ");
+            for (String line : lines) {
+                String[] parts = line.split(",");
+                String bookId = parts[0];
+                String bookName = parts[1];
+                float price = Float.parseFloat(parts[2]);
+
+                if (price >= minPrice && price <= maxPrice) {
+                    System.out.printf("BookId: %-25s BookName: %-25s Price: %-25.1f\n", bookId, bookName, price);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Lỗi: " + e.getMessage());
         }
     }
+    // Đọc file demo.txt và in ra các sách có giá trong khoảng 10000 đến 20000
 }
